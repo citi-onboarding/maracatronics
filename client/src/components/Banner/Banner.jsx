bf6f48e045bf8e319e6d3623a337b49198a5cfd4
Gabriel Melo <xgabrielgomesmelo@gmail.com>
2020-12-03 12:39:15 -0300

    feature(banner-section): add conextion with back-end

--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -1,10 +1,28 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './Banner.css';
 
 function Banner() {
+  const [banner, setBanner] = useState([]);
+
+  const loadBanner = async () => {
+    const res = await axios.get('http://localhost:3001/api/banner');
+    setBanner(res.data);
+  };
+
+  useEffect(() => {
+    loadBanner();
+  }, []);
+
   return (
     <div className="banner">
+      {banner?.map(({_id, image, key}) => (
+      <img key={_id} src={image?.secure_url}/>
+      ))}
     </div>
   );
+
 }
 
 export default Banner;
