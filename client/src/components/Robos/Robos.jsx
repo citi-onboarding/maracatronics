8f00918bb8d64a12d45f24c5ddaca9976e2b906c
Gabriel Melo <xgabrielgomesmelo@gmail.com>
2020-12-09 23:14:14 -0300

    feature(robots-section): edit carousel and fix responsivity

--- client/src/components/Robos/Robos.jsx
+++ client/src/components/Robos/Robos.jsx
@@ -13,7 +13,12 @@ function Robos() {
     const loadRobos = async () => {
         const res = await axios.get('http://localhost:3001/api/robos');
         robosType = res.data
-        setRobos(res.data.dataRoboCombate)
+        setRobos(res.data.dataRoboAutonomoColetivo)
+    }
+
+    function buttonsInitial(){
+        var botaoAutonomosColetivos = document.querySelector('#botaoAutonomosColetivos')
+        botaoAutonomosColetivos.style.backgroundColor = 'yellow'
     }
 
     function buttonsClick(){
@@ -22,33 +27,52 @@ function Robos() {
         var botaoAutonomosColetivos = document.querySelector('#botaoAutonomosColetivos')
 
         const handleBotaoCombateClick = async () => {
+            botaoCombate.style.backgroundColor = 'yellow'
+            botaoAutonomosIndividuais.style.backgroundColor = 'white'
+            botaoAutonomosColetivos.style.backgroundColor = 'white'
             setRobos(robosType.dataRoboCombate)
         }
         botaoCombate.addEventListener('click', handleBotaoCombateClick);
+        botaoCombate.addEventListener('load', handleBotaoCombateClick);
 
         const handleBotaoAutonomosColetivosClick = async () => {
+            botaoCombate.style.backgroundColor = 'white'
+            botaoAutonomosIndividuais.style.backgroundColor = 'white'
+            botaoAutonomosColetivos.style.backgroundColor = 'yellow'
             setRobos(robosType.dataRoboAutonomoColetivo)
         }
         botaoAutonomosColetivos.addEventListener('click', handleBotaoAutonomosColetivosClick);
 
         const handleBotaoAutonomosIndividuaisClick = async () => {
+            botaoCombate.style.backgroundColor = 'white'
+            botaoAutonomosIndividuais.style.backgroundColor = 'yellow'
+            botaoAutonomosColetivos.style.backgroundColor = 'white'
             setRobos(robosType.dataRoboAutonomoIndividual)
         }
         botaoAutonomosIndividuais.addEventListener('click', handleBotaoAutonomosIndividuaisClick);
     }
     
     useEffect(() => {
+        buttonsInitial();
         buttonsClick();
         loadRobos();
     }, []);
 
-    var settings = {
+    const settings = {
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: true
+        arrows: true,
+        responsive: [
+            {
+              breakpoint: 820,
+              settings: {
+                dots: false
+              },
+            },
+          ],
       };
 
     console.log(robos)
@@ -58,26 +82,31 @@ function Robos() {
           <div className="content">
               <h1>Nossos robôs</h1>
               <div className="buttonsSection">
-                  <button id="botaoCombate" >Combate</button>
-                  <button id="botaoAutonomosIndividuais">Autônomos Individuais</button>
                   <button id="botaoAutonomosColetivos">Autônomos Coletivos</button>
+                  <button id="botaoAutonomosIndividuais">Autônomos Individuais</button>
+                  <button id="botaoCombate" >Combate</button>
               </div>
+              <button className="prev" onClick={() => {
+            document.querySelector(".slick-prev.slick-arrow").click();
+          }}></button>
               <div className="carousel">
                   <Slider {...settings}>
-                      {robos?.map(({_id, nome, imagem}) =>(
-
+                      {robos?.map(({_id, nome, imagem, descrição}) =>(
                       <div className="card">
-                          <h2 className="nomeDoRobo">{nome}</h2>
                           <div className="cardText">
-                              <p>O robô Megatron é um robô da franquia transformers, que é péssima por sinal.</p>
+                              <h2 className="nomeDoRobo">{nome}</h2>
+                              <p>{descrição}</p>
                           </div>
-                          <div className="cardImage">
-                              <img src={imagem.secure_url}/>
+                          <div className="cardImage" style={{backgroundImage: `url(${imagem.secure_url})`}}>
+
                           </div>
                       </div>
                       ))}
                   </Slider>
               </div>
+              <button className="next" onClick={() => {
+            document.querySelector(".slick-next.slick-arrow").click();
+          }}></button>
           </div>
       </section>
   );
